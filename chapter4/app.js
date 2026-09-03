757cb1f9ab9a1649db42d8a70801f9e29f359985
tayyabamamin149-sudo <tayyabamamin149@gmail.com>
2026-09-03 19:16:21 +0500

    chapter4

---
+++ chapter4/app.js
@@ -0,0 +1,32 @@
+// Chapter4
+
+// Question1
+// var name, age, city;
+
+// Question2
+//  document.write("<h2>5 Legal Variable Names</h2>");
+
+//     document.write("myName<br>");
+//     document.write("age<br>");
+//     document.write("student_1<br>");
+//     document.write("$price<br>");
+//     document.write("_city<br>");
+
+//     document.write("<h2>5 Illegal Variable Names</h2>");
+
+//     document.write("1name<br>");
+//     document.write("my-name<br>");
+//     document.write("student name<br>");
+//     document.write("123abc<br>");
+//     document.write("var<br>");
+
+// Question3
+//   document.write("<h3>Rules for naming JS variables</h3>");
+
+//     document.write("Variable names can only contain letters, numbers, $ and _. For example $my_1stVariable<br>");
+
+//     document.write("Variable names must begin with a letter, $ or _. For example $name, _name or name<br>");
+
+//     document.write("Variable names are case sensitive<br>");
+
+//     document.write("Variable names should not be JS keywords");
