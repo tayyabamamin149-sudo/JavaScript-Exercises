da1815e544034da5b57cdb5207028144bbf5f9e3
tayyabamamin149-sudo <tayyabamamin149@gmail.com>
2026-09-03 19:14:46 +0500

    Chapter1

---
+++ chapter1/app.js
@@ -0,0 +1,32 @@
+// chapter1
+
+// Question1
+// alert("Welcome to my website!");
+
+// Question2
+// alert("Error! Please enter a valid password.");
+
+// Question3
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+// Question4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// Question5
+// console.log("Hello... I can run JS through my web browser's console");
+
+// Question6
+// alert("Welcome to my website!");
+
+// Question7(a)
+// alert("JavaScript is in the Head!");
+
+// Question7(b)
+// alert("JavaScript is in the Body!");
+
+// Question7(c)
+// alert("JavaScript is inside the HTML!");
+
+// Question7(d)
+// alert("JavaScript is after the HTML!");
