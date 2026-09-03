1e2880a4ce99dd14690c4d13cf9e40eee9672204
tayyabamamin149-sudo <tayyabamamin149@gmail.com>
2026-09-03 19:15:19 +0500

    chapter2

---
+++ chapter2/app.js
@@ -0,0 +1,45 @@
+// Chapter2
+
+// Question1
+// var username;
+
+// Question2
+// var myName = "Tayyaba Amin";
+
+// Question3
+//  var message;
+//     message = "Hello World";
+
+//     alert(message);
+
+// Question4
+//   var studentName = "Tayyaba Amin";
+//     var studentAge = "18 years old";
+//     var studentCourse = "Certified Mobile Application Development";
+
+//     alert(studentName);
+//     alert(studentAge);
+//     alert(studentCourse);
+
+// Question5
+// var word = "PIZZA";
+//     alert(word + "\n" + word.slice(0,4) + "\n" + word.slice(0,3) + "\n" + word.slice(0,2) + "\n" + word.slice(0,1));
+
+
+// Question6
+//   var email = "Tayyabamamin149@gamil.com";
+
+//     alert("My email address is " + email);
+
+// Question7
+//   var book = "A smarter way to learn JavaScript";
+
+//     alert("I am trying to learn from the Book " + book);
+
+// Question8
+//  document.write("<h3>Yah! I can write HTML content through JavaScript</h3>");
+
+// Question9
+//   var designString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//         alert(designString);
+//         document.write(designString)
