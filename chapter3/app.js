93cc93eff25c05b7807c1e9738c3a917785c506f
tayyabamamin149-sudo <tayyabamamin149@gmail.com>
2026-09-03 19:15:52 +0500

    chapter3

---
+++ chapter3/app.js
@@ -0,0 +1,29 @@
+// Chapter3
+
+// Question1
+//  var age = 18;
+//     alert("I am " + age + " years old");
+
+// Question2
+// var visitCount = 15;
+
+//     alert("You have visited this site " + visitCount + " times");
+
+
+// Question3
+//  var birthYear = 2007;
+
+//     document.write("My birth year is " + birthYear);
+
+
+// Question4
+//   var visitorName = "John Doe";
+//     var productTitle = "T-shirt";
+//     var quantity = 5;
+
+//     document.write(
+//         "<b>" + visitorName + "</b>" + " ordered " + "<b>"+ quantity +
+//         productTitle + "</b>" + "(s) on XYZ Clothing store"
+//     );
+       
+
